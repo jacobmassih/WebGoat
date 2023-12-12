6d78fee19bade2bc86932077736fcfe29a111b71
adam.360 <adam.360@hotmail.fr>
2023-12-11 19:43:35 -0500

    changing regions

--- terraform/aws.tf
+++ terraform/aws.tf
@@ -20,7 +20,7 @@ resource "aws_key_pair" "terraformkey" {
 
 # create vpc
 resource "aws_vpc" "k8s_vpc" {
-  cidr_block       = "10.0.0.0/16"
+  cidr_block = "10.0.0.0/16"
   enable_dns_hostnames = true
   enable_dns_support = true
   tags = {
@@ -30,10 +30,10 @@ resource "aws_vpc" "k8s_vpc" {
 
 # Create Subnet
 resource "aws_subnet" "public_subnet" {
-  vpc_id     = aws_vpc.k8s_vpc.id
+  vpc_id = aws_vpc.k8s_vpc.id
   cidr_block = "10.0.1.0/24"
   map_public_ip_on_launch = true
-  availability_zone = "ap-south-1a"
+  availability_zone = "us-east-1a"
   tags = {
     Name = "Public Subnet"
   }
